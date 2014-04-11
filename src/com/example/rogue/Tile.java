29ffea60ff1affcc412633b66d32f7916f3c95c0
Draesia <ramsden.ryan@live.com>
2014-04-11 21:21:10 +0100

    GitIgnore Update

---
+++ src/com/example/rogue/Tile.java
@@ -0,0 +1,42 @@
+package com.example.rogue;
+
+import android.graphics.Color;
+
+public class Tile {
+	
+	public Color color;
+	public int x = 0;
+	public int y = 0;
+	
+	public Tile(Color color, int x, int y)
+	{
+		this.color = color;
+		this.x = x;
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
